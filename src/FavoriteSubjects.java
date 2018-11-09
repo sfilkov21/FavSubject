4851a0d5c50fdc98ff78d689c49ab16fe25ea1c6
sfilkov21 <43067397+sfilkov21@users.noreply.github.com>
2018-11-09 17:42:15 -0500

    Add files via upload

---
+++ src/FavoriteSubjects.java
@@ -0,0 +1,12 @@
+
+public class FavoriteSubjects {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("My facvorite subjects: ");
+		System.out.print("\n\tComputer Science");
+		System.out.print("\n\tMath");
+		System.out.print("\n\tHistory");
+	}
+
+}
